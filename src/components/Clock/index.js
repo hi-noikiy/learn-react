289d3642b2b2e85b7de0d9a3e4be5e41a8cad3f3
zhang-xiao-miao <1286874200@qq.com>
2018-08-11 08:22:50 -0400

    handle even

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './index.scss'
+import './index.css'
 
 class Clock extends Component {
   // Class components should always call the base constructor with props.
@@ -7,39 +7,72 @@ class Clock extends Component {
     super(props)
     // State is similar to props, but it is private and fully controlled by the component.
     this.state = {
-      date: new Date()
+      date: new Date(),
+      active: true
     }
   }
 
   // When a component output is inserted in the DOM, react calls the componentDidMount life circle hooks.
   componentDidMount() {
-    this.timerId = setInterval(() => {
-      /* Every second the browser calls the tick () method. Inside it, the Clock component schedules a UI update by calling setState.
-      with an object contains current time.
-      */
-      this.tick()
-    }, 1000)
+    this.activateTimer()
   }
   
-  // f the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle hook so the timer is stopped
+  // if the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle hook so the timer is stopped
   componentWillUnmount () {
     clearInterval(this.timerId)
     this.timerId = null
   }
 
+  activateTimer () {
+    this.timerId = setInterval(() => {
+      /* Every second the browser calls the tick () method. Inside it, the Clock component schedules a UI update by calling setState.
+      with an object contains current time.
+      */
+      this.tick()
+    }, 1000) 
+  }
+
   tick () {
-    // The Tick
+    // schedule an update by setState
     this.setState({
       date: new Date()
     })
   }
+
+  // e is an instance of SyntheticEvent
+  toggleTimer (e) {
+    // console.log(this, e)
+    // call manually rather than return false expression
+    e.preventDefault()
+    e.stopPropagation()
+
+    let isActive
+
+    if (this.timerId) {
+      clearInterval(this.timerId)
+      this.timerId = null
+      isActive = false
+    } else {
+      this.activateTimer()
+      isActive = true
+    }
+
+    this.setState((prevState, props) => {
+      return {
+        active: isActive
+      }
+    })
+  }
+
   // render function returns an instance of React class
+  // Whenever is a state change, render function is claled by React.
   render () {
-    // Whenever is a state change, render function is claled by React.
+    let btnText = this.state.active ? 'Off' : 'On'
     return (
-      <div className="Tick">
+      <div className="Clock">
         <h1>Hello, world!</h1>
         <h2>It's {this.state.date.toLocaleTimeString()}</h2>
+        <button className="btn" onClick={this.toggleTimer.bind(this)}>{btnText}</button>
       </div>
     )
   }
