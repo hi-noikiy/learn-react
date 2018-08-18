bb43b2f6f6b563af639e5d8deb1669c5a8c7690d
zhang-xiao-miao <1286874200@qq.com>
2018-08-18 01:42:15 -0400

    add not data hint

--- src/redux/blog/components/Posts.js
+++ src/redux/blog/components/Posts.js
@@ -1,30 +1,50 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 import PropTypes from 'prop-types'
+
 import Post from './Post'
 
 import './Posts.css'
 
-function Posts (props) {
-  let posts = props.posts
+class Posts extends React.Component {
+
+  componentWillMount () {
+    this.props.fetchPosts()
+  }
+
+  render () {
+    let posts = this.props.posts
     let postList = posts.map((post, index) => {
       return (
         <li key={post.id}>
-          <Post title={post.title} content={post.body} />
+          <Post title={post.title} content={post.body} id={post.id} />
         </li>
       )
     })
+
     return (
       <div className="Posts-wrapper">
         <h2 className="title">Posts</h2>
         <ul className="Posts-list">
-          {postList}
+          {postList.length ? postList : <p className="no-post">Not Post Now</p>}
         </ul>
       </div>
     )
+  }
 }
 
 Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
   posts: PropTypes.array.isRequired
 }
 
-export default Posts
+function mapStateToProps (state) {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts })(Posts)
