c219a4b547dcf9bfcff7482546d0fd158eca4931
zhang-xiao-miao <1286874200@qq.com>
2018-08-18 01:39:35 -0400

    fix bug: remove posts as a props of Posts component

--- src/redux/blog/index.js
+++ src/redux/blog/index.js
@@ -11,7 +11,7 @@ class App  extends Component {
      <div className="App">
         <div className="App-content">
           <PostForm />
-          <Posts posts={posts}/>
+          <Posts />
         </div>
      </div>
     )
