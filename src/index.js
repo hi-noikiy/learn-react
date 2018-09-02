a8be02c452e840352328c5ed038e96395602c927
zhang-xiao-miao <1286874200@qq.com>
2018-09-01 20:13:32 -0400

    test redux demo: post

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker'
-import App from './ReactRouter/demo01/index.js'
+import App from './redux/blog'
 
 
 import './assets/style/common.css'
--- src/redux/blog/index.js
+++ src/redux/blog/index.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 import Posts from './components/Posts'
 import PostForm from './containers/PostForm'
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 import './index.css'
 
 
-class App  extends Component {
+class Root  extends Component {
   render() {
     return (
      <div className="App">
@@ -18,4 +21,12 @@ class App  extends Component {
   }
 }
 
-export default App
+export default class App extends React.Component {
+  render () {
+    return (
+      <Provider store={store}>
+        <Root></Root>
+      </Provider>
+    )
+  }
+}
