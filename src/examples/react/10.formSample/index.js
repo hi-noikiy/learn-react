c8050c2c06b43e4cf6e7438653323dd4ecf571c5
zhang-xiao-miao <1286874200@qq.com>
2018-09-20 19:41:12 +0800

    enhance form example

--- src/examples/react/10.formSample/index.js
+++ src/examples/react/10.formSample/index.js
@@ -9,28 +9,38 @@ export default class FormSample extends Component {
         value: '',
         error: ''
       },
-      password: {
+      remark: {
         value: '',
         error: ''
+      },
+      department: {
+        value: 'Marketing',
+        error: ''
+      },
+      remmber: {
+        value: true
       }
     }
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
   }
 
   componentDidMount () {
     document.title = 'FormSample'
   }
 
-  onchange (e) {
+  onChange(e) {
     let $target = e.target
+    const value = $target.type === 'checkbox' ? $target.checked : $target.value
     this.setState({
       [$target.name]: {
-        value: $target.value,
-        error: ($target.value === '') ? `请输入${$target.name}` : ''
+        value,
+        error: ($target.value === '') ? `Please type in${$target.name}` : ''
       }
     })
   }
 
-  onsubmit (e) {
+  onSubmit (e) {
     e.preventDefault()
     let hasError = false
     Object.keys(this.state).forEach(key => {
@@ -59,11 +69,11 @@ export default class FormSample extends Component {
           <h4 className="FormSample-title">A Form demo</h4>
         </header>
         <div className="FormSample-main">
-          <form className="form" onSubmit={this.onsubmit.bind(this)} ref={this.form}>
+          <form className="form" onSubmit={this.onSubmit}>
             <div className="form-group">
               <p className="form-input">
-                <label htmlFor="username" className="lable">username</label>
-                <input type="text" name="username" id="username" className="form-control" onChange={this.onchange.bind(this)} />
+                <label htmlFor="username" className="label">Username:</label>
+                <input type="text" name="username" id="username" className="form-control" onChange={this.onChange} />
               </p>
               {
                 this.state['username'].error ? <p className="field-error">{this.state['username'].error}</p> : null
@@ -71,12 +81,34 @@ export default class FormSample extends Component {
             </div>
             <div className="form-group">
               <p className="form-input">
-                <label htmlFor="password" className="lable">password</label>
-                <input type="password" name="password" id="password" className="form-control" onChange={this.onchange.bind(this)} />
+              <label htmlFor="remarks" className="label">Remark:</label>
+              <textarea name="remark" id="remark" className="form-control" onChange={this.onChange} value={this.state.remark.value}></textarea>
               </p>
               {
-                this.state['password'].error ? <p className="field-error">{this.state['password'].error}</p> : null
+                this.state['remark'].error ? <p className="field-error">{this.state['remark'].error}</p> : null
               }
+            </div>
+            <div className="form-group">
+              <p className="form-input">
+                <label htmlFor="department" className="label">Department:</label>
+              </p>
+              <p className="form-control">
+                <select name="department" id="department" className="form-control" onChange={this.onChange} value={this.state.department.value}>
+                  <option value="">Please Select department</option>
+                  <option value="Tech">Tech</option>
+                  <option value="Marketing">Marketing</option>
+                  <option value="Office">Office</option>
+                </select>
+              </p>
+              {
+                this.state['department'].error ? <p className="field-error">{this.state['department'].error}</p> : null
+              }
+            </div>
+            <div className="form-group">
+              <p className="form-input">
+                <label htmlFor="remmber">remmber:</label>
+                <input type="checkbox" name="remmber" id="remmber" checked={this.state.remmber.value} onChange={this.onChange}/>
+              </p>
             </div>
             <div className="form-group">
               <p className="form-input  button-group">
